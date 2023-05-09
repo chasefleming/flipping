7867c28d647f1d3ad5b9133d5fe8801a3dc8f7e9
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2023-05-09 12:35:36 -0700

    Adds tip selector to tip modal

--- src/components/TipModal.js
+++ src/components/TipModal.js
@@ -1,14 +1,20 @@
-import React, { useCallback, useEffect, useRef } from "react"
+import React, { useCallback, useState, useEffect, useRef } from "react"
 import ReactDOM from "react-dom"
 import * as fcl from "@onflow/fcl"
 
 export default function TipModal({ address, amount, handleClose, isOpen }) {
+  const [selectedAmount, setSelectedAmount] = useState(amount)
+  const [selectedAddress, setSelectedAddress] = useState(address)
+
   const executeTransaction = useCallback(async () => {
     await fcl.mutate({
       template: "https://flix.flow.com/v1/templates?name=transfer-flow",
-      args: (arg, t) => [arg(amount, t.UFix64), arg(address, t.Address)],
+      args: (arg, t) => [
+        arg(selectedAmount, t.UFix64),
+        arg(selectedAddress, t.Address),
+      ],
     })
-  }, [address, amount])
+  }, [selectedAddress, selectedAmount])
 
   return ReactDOM.createPortal(
     <div
@@ -17,6 +23,25 @@ export default function TipModal({ address, amount, handleClose, isOpen }) {
       className="modal-wrapper"
     >
       <div className="modal-inner" onClick={(e) => e.stopPropagation()}>
+        <p>
+          <div className="modal-row">
+            {"Tip: "}
+            <input
+              value={selectedAmount}
+              onChange={(e) => setSelectedAmount(e.target.value)}
+            ></input>
+            {" FLOW"}
+          </div>
+        </p>
+        <p>
+          <div className="modal-row">
+            {"To: "}
+            <input
+              value={selectedAddress}
+              onChange={(e) => setSelectedAddress(e.target.value)}
+            ></input>
+          </div>
+        </p>
         <button onClick={executeTransaction}>Send Tip</button>
       </div>
     </div>,
